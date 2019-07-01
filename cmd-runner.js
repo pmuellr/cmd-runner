33df56fa370a26ae497328a977185b290a6e6b4d
Patrick Mueller <pmuellr@gmail.com>
2019-06-30 20:18:00 -0400

    add GET /api/commands endpoint

--- cmd-runner.js
+++ cmd-runner.js
@@ -2,10 +2,49 @@
 
 'use strict'
 
-const logger = require('./lib/logger').createLogger(__filename)
+module.exports = {
+  main
+}
+
 const Config = require('./lib/config')
+const Server = require('./lib/server')
+const logger = require('./lib/logger').createLogger(__filename)
 
-logger.debug('starting')
+// @ts-ignore
+if (require.main === module) main()
 
-const config = Config.readConfig('./cmd-runner.toml')
-console.log(JSON.stringify(config, null, 4))
+async function main () {
+  logger.debug('starting')
+
+  const config = Config.readConfig('./cmd-runner.toml')
+  logger.debug(`config: ${JSON.stringify(config, null, 4)}`)
+
+  let stopServer
+  try {
+    stopServer = await Server.startServer({
+      host: config.host,
+      port: config.port,
+      config
+    })
+  } catch (err) {
+    process.exit(1)
+  }
+
+  process.on('SIGINT', handleSignal)
+  process.on('SIGTERM', handleSignal)
+
+  async function handleSignal (signal) {
+    console.log('')
+    logger.warn(`received signal ${signal}, shutting down`)
+
+    try {
+      await stopServer()
+    } catch (err) {
+      logger.error(`error stopping server: ${err.message}`)
+      process.exit(1)
+    }
+
+    logger.info(`exiting`)
+    process.exit(0)
+  }
+}
